88aa73391831f63d56e4ce0b0b77913c9a1652fa
pratik0809 <pratikkbirla@gmail.com>
2025-02-15 15:30:08 +0530

    First Commit - Youtube Clone

---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: '50'
+  },
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_API_KEY || 'b1b4806f65mshf2912bae8c34ab4p1d67d8jsn150a2d91594b',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+export const api = async (url) => {
+  const { data } = await axios.get(`${URL}/${url}`, options);
+
+  return data;
+};
